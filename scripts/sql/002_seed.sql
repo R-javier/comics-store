4807104e7a7bffdd363470ec3191362281fd692a
Javier Rodriguez <javiplus.-@live.com.ar>
2026-02-10 14:47:58 -0300

    Agrego .dockerignore tambien cambios en docker compose y database

---
+++ scripts/sql/002_seed.sql
@@ -0,0 +1,5 @@
+INSERT INTO comics (title, issue_number, publisher, price, stock) VALUES
+('The Amazing Spider-Man', 1, 'Marvel', 5.99, 10),
+('Batman', 50, 'DC', 6.50, 8),
+('Saga', 1, 'Image', 9.99, 5);
+
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { DatabaseModule } from './database/database.module';
 import { ComicsModule } from './comics/comics.module';
 import { CustomersModule } from './customers/customers.module';
 import { PurchasesModule } from './purchases/purchases.module';
 
 @Module({
-  imports: [DatabaseModule, ComicsModule, CustomersModule, PurchasesModule],
+  imports: [ComicsModule, CustomersModule, PurchasesModule],
   controllers: [AppController],
   providers: [AppService],
 })
