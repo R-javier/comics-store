33d26adad68e6aca944fed435c0a2a7d84d248b0
Javier Rodriguez <javiplus.-@live.com.ar>
2026-02-12 12:16:08 -0300

    Agrego test e2e de comics

---
+++ test/comics.e2e-spec.ts
@@ -0,0 +1,43 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { INestApplication } from '@nestjs/common';
+import  request from 'supertest';
+import { AppModule } from '../src/app.module';
+
+
+describe('Comics endpoints (e2e)', () => {
+    let app: INestApplication;
+
+    beforeAll(async() => {
+        const module: TestingModule = await Test.createTestingModule({
+            imports: [AppModule],
+        }).compile();
+
+        app = module.createNestApplication();
+        await app.init();
+    });
+
+    afterAll(async () => {
+        await app.close();
+    });
+
+    it('GET /comics', async () => {
+        const res = await request(app.getHttpServer()).get('/comics');
+        expect(res.status).toBe(200);
+        expect(res.text).toContain('Traigo todos los comics');
+    })
+
+    it('POST /comics/:id', async () => {
+        const res = await request(app.getHttpServer())
+        .post('/comics')
+        .send({ title: 'Batman #1' })
+        expect(res.status).toBe(201)
+        expect(res.text).toContain('Agrego comic')
+    });
+
+    it('DELETE /comics/:id', async() => {
+        const res = await request(app.getHttpServer())
+        .delete('/comics/1')
+        expect(res.status).toBe(200)
+        expect(res.text).toContain('borro comic')
+    });
+});
